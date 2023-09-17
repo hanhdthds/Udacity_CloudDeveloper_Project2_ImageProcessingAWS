9d9453d983b010940c82f2deeb71a00f2230c3d8
Pinky <hanhdthds@gmail.com>
2023-09-18 01:10:46 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+
+
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url } = req.query;
+    
+    try {
+      const image_file = await filterImageFromURL(image_url);
+
+      res.status(200).sendFile(image_file);
+
+      res.on('finish', () => deleteLocalFiles([image_file]));
+
+    } catch (error) {
+      res.status(422).send("Your image url invalid")
+    }
+  })
+
+  app.get( "/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
